4b86d459e8e27b00a937ee6e9829e87efed45ecd
Rachel Fischoff <rfischoff@gmail.com>
2021-10-05 19:45:01 -0400

    feat: image of plants rendering in boxes with alt id as the botancial_name

--- src/components/PlantsResults.js
+++ src/components/PlantsResults.js
@@ -1,10 +1,6 @@
 import React, { useContext } from "react";
 import { PlantContext } from "../context/PlantContext";
-import {
-  SimpleGrid,
-  Box,
-  Text,
-} from "@chakra-ui/react";
+import { SimpleGrid, Box, Text, Image } from "@chakra-ui/react";
 
 export default function PlantsResults() {
   const { filteredPlants } = useContext(PlantContext);
@@ -16,6 +12,12 @@ export default function PlantsResults() {
           //change about to id. add ids to database.
           <Box w="100%" h="40" bg="blue.500" key={plant.about}>
             <Text>{plant.common_name}</Text>
+            <Image
+              boxSize="100px"
+              objectFit="cover"
+              src={plant.image_url}
+              alt={plant.botanical_name}
+            ></Image>
           </Box>
         ))}
     </SimpleGrid>
