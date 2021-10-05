9406f065b92384007d5f814b5529af441d7a7c89
Rachel Fischoff <rfischoff@gmail.com>
2021-10-05 18:33:37 -0400

    feat: search input is set to state via handleSearch function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { PlantContext } from "../context/PlantContext";
 import { Box, Text, Input } from "@chakra-ui/react";
 
 export default function Search() {
   const { plantList } = useContext(PlantContext);
+
+  //Should it be an empty string? should it be in the search section or the context?
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSearch = (event) => setSearchTerm(event.target.value);
+  
+
   return (
-    <Box borderRadius="lg"  p="6" w="75%">
-      <Input placeholder="Search for Available Plants"></Input>{" "}
+    <Box borderRadius="lg" p="6" w="75%">
+      <Input
+        placeholder="Search for Available Plants"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
     </Box>
   );
 }
