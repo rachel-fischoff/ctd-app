59aa32a06b8a5cb9baff00d6749b36f049b22a4c
Rachel Fischoff <rfischoff@gmail.com>
2021-10-05 20:20:38 -0400

    fix: deleted unneccessary code lines

--- src/components/PlantsResults.js
+++ src/components/PlantsResults.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { PlantContext } from "../context/PlantContext";
 import {
   SimpleGrid,
@@ -15,18 +15,11 @@ import {
 export default function PlantsResults() {
   const { filteredPlants } = useContext(PlantContext);
 
-
-//   const [value, setValue] = useState(defaultValue);
-
   /* In this component? 
     1) save button? that updates the database based on the list - 
     2) another component that checks for inventory updates and emails the department
   */
-  const handleInventoryUpdate = () => {
-
-
-
-  };
+  const handleInventoryUpdate = () => {};
 
   return (
     <SimpleGrid minChildWidth="200px" gap={6}>
@@ -43,7 +36,7 @@ export default function PlantsResults() {
             ></Image>
             <Text>Number in Stock </Text>
             {/* TODO: add inventory field to database and then value = plant.available_to_sell */}
-            <NumberInput size="md" maxW={24} onChange={handleInventoryUpdate} >
+            <NumberInput size="md" maxW={24} onChange={handleInventoryUpdate}>
               <NumberInputField />
               <NumberInputStepper>
                 <NumberIncrementStepper />
