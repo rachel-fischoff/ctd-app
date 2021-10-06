e1dd146c6ebd622666d3be833b0271d1223778f0
Rachel Fischoff <rfischoff@gmail.com>
2021-10-05 20:04:20 -0400

    feat: added a number input to the each plant result box

--- src/components/PlantsResults.js
+++ src/components/PlantsResults.js
@@ -1,16 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { PlantContext } from "../context/PlantContext";
-import { SimpleGrid, Box, Text, Image } from "@chakra-ui/react";
+import {
+  SimpleGrid,
+  Box,
+  Text,
+  Image,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from "@chakra-ui/react";
 
 export default function PlantsResults() {
   const { filteredPlants } = useContext(PlantContext);
+
+
+//   const [value, setValue] = useState(defaultValue);
+
+  /* In this component? 
+    1) save button? that updates the database based on the list - 
+    2) another component that checks for inventory updates and emails the department
+  */
+  const handleInventoryUpdate = () => {
+
+
+
+  };
 
   return (
     <SimpleGrid minChildWidth="200px" gap={6}>
       {filteredPlants &&
         filteredPlants.map((plant) => (
           //change about to id. add ids to database.
-          <Box w="100%" h="40" bg="blue.500" key={plant.about}>
+          <Box w="100%" h="60" bg="gray.400" key={plant.about}>
             <Text>{plant.common_name}</Text>
             <Image
               boxSize="100px"
@@ -18,6 +41,15 @@ export default function PlantsResults() {
               src={plant.image_url}
               alt={plant.botanical_name}
             ></Image>
+            <Text>Number in Stock </Text>
+            {/* TODO: add inventory field to database and then value = plant.available_to_sell */}
+            <NumberInput size="md" maxW={24} onChange={handleInventoryUpdate} >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
           </Box>
         ))}
     </SimpleGrid>
